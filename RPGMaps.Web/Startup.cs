731958fb0e3d1fdff01e9c5b66ba3d52ed83b06a
dpinto <dpinto@CMC22922>
2017-09-20 11:13:13 +0100

    Add project files.

---
+++ RPGMaps.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGMaps.Web.Startup))]
+namespace RPGMaps.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ RPGMaps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RPGMaps.MvcApplication" Language="C#" %>
